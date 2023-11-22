725ab75dcdef3a13ef448afc33fa9858d1422a8a
bornartem <born.artem@gmail.com>
2023-11-22 16:43:22 +0300

    2 commit

--- src/main/java/com/example/TestCalculator/TestCalculatorApplication.java
+++ src/main/java/com/example/TestCalculator/TestCalculatorApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestCalculatorApplication {
+public class 	TestCalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestCalculatorApplication.class, args);
